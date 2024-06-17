df3750ec9c1f7cccbae5d45a70181d4ad938f621
Kaupenjoe <N.Kaupenjohann@gmail.com>
2024-06-17 11:21:59 +0200

    items and item group

--- src/main/java/net/kaupenjoe/mccourse/MCCourseMod.java
+++ src/main/java/net/kaupenjoe/mccourse/MCCourseMod.java
@@ -2,16 +2,19 @@ package net.kaupenjoe.mccourse;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.kaupenjoe.mccourse.item.ModItemGroups;
+import net.kaupenjoe.mccourse.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// Very important commment!
 public class MCCourseMod implements ModInitializer {
 	public static final String MOD_ID = "mccourse";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
 
+		ModItems.registerModItems();
 	}
 }
