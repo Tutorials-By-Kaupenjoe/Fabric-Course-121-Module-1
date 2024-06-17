414f1552ec0f45ff93bb68aca2e3d3560c010cdc
Kaupenjoe <N.Kaupenjohann@gmail.com>
2024-06-17 10:25:50 +0200

    Initial commit

---
+++ src/main/java/net/kaupenjoe/mccourse/MCCourseMod.java
@@ -0,0 +1,17 @@
+package net.kaupenjoe.mccourse;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// Very important commment!
+public class MCCourseMod implements ModInitializer {
+	public static final String MOD_ID = "mccourse";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
