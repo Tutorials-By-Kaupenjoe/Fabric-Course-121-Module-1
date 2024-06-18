53216fc12dd396331a6233f1ae31c8635aabaf5a
Kaupenjoe <N.Kaupenjohann@gmail.com>
2024-06-18 13:59:30 +0200

    button and pressure plate

--- src/main/java/net/kaupenjoe/mccourse/block/ModBlocks.java
+++ src/main/java/net/kaupenjoe/mccourse/block/ModBlocks.java
@@ -39,6 +39,11 @@ public class ModBlocks {
     public static final Block FLUORITE_SLAB = registerBlock("fluorite_slab",
             new SlabBlock(AbstractBlock.Settings.create().strength(2f).requiresTool()));
 
+    public static final Block FLUORITE_BUTTON = registerBlock("fluorite_button",
+            new ButtonBlock(BlockSetType.IRON, 10, AbstractBlock.Settings.create().requiresTool()));
+    public static final Block FLUORITE_PRESSURE_PLATE = registerBlock("fluorite_pressure_plate",
+            new PressurePlateBlock(BlockSetType.IRON, AbstractBlock.Settings.create().requiresTool()));
+
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
