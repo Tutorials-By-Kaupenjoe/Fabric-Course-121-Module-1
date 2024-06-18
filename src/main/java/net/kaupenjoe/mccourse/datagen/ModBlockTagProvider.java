b8452409aad7411b6167112eef51a62fabd5d806
Kaupenjoe <N.Kaupenjohann@gmail.com>
2024-06-18 13:11:46 +0200

    datagen

---
+++ src/main/java/net/kaupenjoe/mccourse/datagen/ModBlockTagProvider.java
@@ -0,0 +1,32 @@
+package net.kaupenjoe.mccourse.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.kaupenjoe.mccourse.block.ModBlocks;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.BlockTags;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
+    public ModBlockTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+        getOrCreateTagBuilder(BlockTags.PICKAXE_MINEABLE)
+                .add(ModBlocks.FLUORITE_BLOCK,
+                        ModBlocks.FLUORITE_ORE,
+                        ModBlocks.FLUORITE_DEEPSLATE_ORE,
+                        ModBlocks.FLUORITE_END_ORE,
+                        ModBlocks.FLUORITE_NETHER_ORE);
+
+        getOrCreateTagBuilder(BlockTags.NEEDS_DIAMOND_TOOL)
+                .add(ModBlocks.FLUORITE_END_ORE,
+                        ModBlocks.FLUORITE_NETHER_ORE);
+
+        getOrCreateTagBuilder(BlockTags.NEEDS_IRON_TOOL)
+                .add(ModBlocks.FLUORITE_DEEPSLATE_ORE);
+    }
+}
---
+++ src/main/java/net/kaupenjoe/mccourse/datagen/ModModelProvider.java
@@ -0,0 +1,37 @@
+package net.kaupenjoe.mccourse.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.kaupenjoe.mccourse.block.ModBlocks;
+import net.kaupenjoe.mccourse.item.ModItems;
+import net.minecraft.data.client.BlockStateModelGenerator;
+import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.Models;
+
+public class ModModelProvider extends FabricModelProvider {
+    public ModModelProvider(FabricDataOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.FLUORITE_BLOCK);
+
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.FLUORITE_ORE);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.FLUORITE_DEEPSLATE_ORE);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.FLUORITE_END_ORE);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.FLUORITE_NETHER_ORE);
+
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.MAGIC_BLOCK);
+    }
+
+    @Override
+    public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+        itemModelGenerator.register(ModItems.FLUORITE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.RAW_FLUORITE, Models.GENERATED);
+
+        itemModelGenerator.register(ModItems.CHAINSAW, Models.GENERATED);
+        itemModelGenerator.register(ModItems.STRAWBERRY, Models.GENERATED);
+        itemModelGenerator.register(ModItems.STARLIGHT_ASHES, Models.GENERATED);
+    }
+}
